a5fdf57a0de6c4d7fc95445132d0ade00f67d4f1
sadikhossain <57746897+sadikhossain@users.noreply.github.com>
2026-06-24 15:52:24 +0600

    Create a simple Web API project

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+// REST ==> GET, POST, PUT, DELETE
+
+app.MapGet("/", () =>
+{
+    return "API is working fine.";
+});
+
+app.MapGet("/hello", () =>
+{
+    return "Get method: Hello";
+});
+
+
+app.MapPost("/hello", () =>
+{
+    return "POST method: Hello";
+});
+
+app.MapPut("/hello", () =>
+{
+    return "PUT method: Hello";
+});
+
+app.MapDelete("/hello", () =>
+{
+    return "DELETE method: Hello";
+});
 
 app.Run();
-
-
