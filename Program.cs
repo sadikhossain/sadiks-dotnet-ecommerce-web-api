044db1eab1148383b4663da0a04c15f57e8a4fd4
sadikhossain <57746897+sadikhossain@users.noreply.github.com>
2026-06-27 16:24:43 +0600

    CRUD operation implementation

--- Program.cs
+++ Program.cs
@@ -15,23 +15,71 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// REST ==> GET, POST, PUT, DELETE
+List<Category> categories = new List<Category>();
 
+// REST ==> GET, POST, PUT, DELETE
 app.MapGet("/", () => "API is working fine.");
 
-var products = new List<Product>()
+// GET  /api/categories ==> Read Categories
+app.MapGet("/api/categories", () =>
 {
-    new Product("Samsung s20", 1250),
-    new Product("Apple iphone", 1367),
-};
-
-app.MapGet("/products", () =>
-{
-
-    return Results.Ok(products); // 200
+    return Results.Ok(categories); // 200
 });
 
+// POST  /api/categories ==> Create Categories
+app.MapPost("/api/categories", () =>
+{
+    var newCategory = new Category
+    {
+        CategoryId = Guid.Parse("d24cdbed-dea9-4416-bdf7-183816a43e63"),
+        Name = "Electronics",
+        Description = "Devices an gadgets including phone, laptop and other accesories",
+        CreatedAt = DateTime.UtcNow,
+    };
+    categories.Add(newCategory);
+    return Results.Created($"/api/categories/{newCategory.CategoryId}", newCategory); // 200
+});
+
+// DELETE  /api/categories ==> Delete a Categories
+app.MapDelete("/api/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(category => category.CategoryId == Guid.Parse("4681c82f-eb5e-435f-9638-b6d2e1d6efcd"));
+    if(foundCategory == null)
+    {
+        return Results.NotFound("Category with this id does not exist");
+    }
+    categories.Remove(foundCategory);
+    return Results.NoContent();
+});
+
+// PUT  /api/categories ==> Update a Categories
+app.MapPut("/api/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(category => category.CategoryId == Guid.Parse("d24cdbed-dea9-4416-bdf7-183816a43e63"));
+    if(foundCategory == null)
+    {
+        return Results.NotFound("Category with this id does not exist");
+    }
+    foundCategory.Name = "Smart Phone";
+    foundCategory.Description = "Smart Phone is a nice category";
+    // categories.Remove(foundCategory);
+    return Results.NoContent();
+});
 
 app.Run();
 
-public record Product(string Name, decimal Price);
+public record Category
+{
+    public Guid CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+};
+
+
+// CRUD
+// Create ==> Create a category ==> POST: /api/category
+// Read ==> Read a category ==> GET: /api/category
+// Update ==> Update a category ==> PUT: /api/category
+// Delete ==> Delete a category ==> DELETE: /api/category
