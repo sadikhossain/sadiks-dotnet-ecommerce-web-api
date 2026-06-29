31576da350dc2f0114df381ffe3609f24dcba216
sadikhossain <57746897+sadikhossain@users.noreply.github.com>
2026-06-29 16:37:20 +0600

    add route constraints to validate routes in get, put methods

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ app.MapPost("/api/categories", ([FromBody] Category categoryData) =>
 });
 
 // DELETE  /api/categories/{categoryId} ==> Delete a Categories
-app.MapDelete("/api/categories/{categoryId}", (Guid categoryId) =>
+app.MapDelete("/api/categories/{categoryId:guid}", (Guid categoryId) =>
 {
     var foundCategory = categories.FirstOrDefault(category => category.CategoryId == categoryId);
     if(foundCategory == null)
@@ -64,7 +64,7 @@ app.MapDelete("/api/categories/{categoryId}", (Guid categoryId) =>
 });
 
 // PUT  /api/categories/{categoryId} ==> Update a Categories
-app.MapPut("/api/categories/{categoryId}", (Guid categoryId, [FromBody] Category categoryData) =>
+app.MapPut("/api/categories/{categoryId:guid}", (Guid categoryId, [FromBody] Category categoryData) =>
 {
     var foundCategory = categories.FirstOrDefault(category => category.CategoryId == categoryId);
     if(foundCategory == null)
